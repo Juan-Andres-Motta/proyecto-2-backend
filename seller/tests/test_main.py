3e0f48c0687e839cfb229fea1d49bff634f65175
Martin Galvan <md.galvan@uniandes.edu.co>
2025-10-13 17:23:17 -0500

    test: fixed failing test

--- seller/tests/test_main.py
+++ seller/tests/test_main.py
@@ -5,12 +5,12 @@ client = TestClient(app)
 
 
 def test_read_root():
-    response = client.get("/")
+    response = client.get("/seller/")
     assert response.status_code == 200
     assert response.json() == {"name": "Seller Service"}
 
 
 def test_read_health():
-    response = client.get("/health")
+    response = client.get("/seller/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
